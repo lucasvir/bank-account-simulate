9005cc209ab437483686b554db7db1644bd600ac
lucasvir <lavirmond@gmail.com>
2024-02-09 15:30:18 -0300

    simulação interativa implementada

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner escaner = new Scanner(System.in);
+
+        System.out.println();
+        System.out.println("****************************************");
+        System.out.println("Simulado de Conta Bancaria");
+        System.out.println("****************************************");
+        System.out.println();
+
+        System.out.print("Por favor, digite o número da conta: ");
+        int conta = escaner.nextInt();
+        System.out.println();
+
+        System.out.print("Por favor, digite o número da agência: ");
+        String agencia = escaner.next();
+        escaner.nextLine();
+        System.out.println();
+
+        System.out.print("Por favor, digite seu nome completo: ");
+        String nome = escaner.nextLine();
+        System.out.println();
+
+        System.out.print("Por favor, digite o saldo inicial da conta: ");
+        double saldo = escaner.nextDouble();
+        System.out.println();
+
+        System.out.printf(
+            "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo R$%.2f já está disponível para saque.\n"
+            , nome, agencia, conta, saldo);
+        System.out.println();
+
+        System.out.println("****************************************");
+        System.out.println("Fim da simulação");
+        System.out.println("****************************************");
+        System.out.println();
+
+        escaner.close();
     }
 }
